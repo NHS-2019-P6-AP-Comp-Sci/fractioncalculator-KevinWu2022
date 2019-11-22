52ccbff75d34bb30420910a53728f5fb426cc075
s-wukev <s-wukev@53S50030681.it.bsd405.org>
2019-11-22 13:43:48 -0800

    2nd checkpoint

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -9,8 +9,16 @@ public class FracCalc {
     {
         // TODO: Read the input from the user and call produceAnswer with an equation
     	Scanner s = new Scanner(System.in);
-    	String input = s.nextLine();
-    	System.out.println(produceAnswer(input));
+    	String input = " ";
+    	while (input.equals("quit") != true) {
+    		input = s.nextLine();
+    		if (input.equals("quit")) {
+    			s.close();
+    		}
+    		else {
+    			System.out.println(produceAnswer(input));
+    		}
+    	}
     }
 
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -26,9 +34,49 @@ public class FracCalc {
         // TODO: Implement this function to produce the solution to the input
     	int space = input.indexOf(" ");
     	String firstOperand = input.substring(0, space);
+    	int firstSlash = firstOperand.indexOf("/");
+    	int firstUnderscore = firstOperand.indexOf("_");
+    	String firstNumerator = "";
+    	String firstDenominator = "";
+    	String firstWhole = "";
+    	if (firstSlash == -1 && firstUnderscore == -1) {
+    		firstNumerator = "0";
+    		firstDenominator = "1";
+        	firstWhole = firstOperand;
+    	}
+    	else if (firstSlash != -1 && firstUnderscore != -1) {
+    		firstNumerator = firstOperand.substring(firstUnderscore + 1, firstSlash);
+    		firstDenominator = firstOperand.substring(firstSlash + 1);
+        	firstWhole = firstOperand.substring(0, firstUnderscore);
+    	}
+    	else if (firstSlash != -1 && firstUnderscore == -1) {
+    		firstNumerator = firstOperand.substring(0, firstSlash);
+    		firstDenominator = firstOperand.substring(firstSlash + 1);
+    		firstWhole = "0";
+    	}
     	String operator = input.substring(space + 1, space + 2);
     	String secondOperand = input.substring(space + 3);
-        return secondOperand;
+    	int secondSlash = secondOperand.indexOf("/");
+    	int secondUnderscore = secondOperand.indexOf("_");
+    	String secondNumerator = "";
+    	String secondDenominator = "";
+    	String secondWhole = "";
+    	if (secondSlash == -1 && secondUnderscore == -1) {
+    		secondNumerator = "0";
+    		secondDenominator = "1";
+        	secondWhole = secondOperand;
+    	}
+    	else if (secondSlash != -1 && secondUnderscore != -1) {
+    		secondNumerator = secondOperand.substring(secondUnderscore + 1, secondSlash);
+    		secondDenominator = secondOperand.substring(secondSlash + 1);
+        	secondWhole = secondOperand.substring(0, secondUnderscore);
+    	}
+    	else if (secondSlash != -1 && secondUnderscore == -1) {
+    		secondNumerator = secondOperand.substring(0, secondSlash);
+    		secondDenominator = secondOperand.substring(secondSlash + 1);
+    		secondWhole = "0";
+    	}
+        return "whole:" + secondWhole + " numerator:" + secondNumerator + " denominator:" + secondDenominator;
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
